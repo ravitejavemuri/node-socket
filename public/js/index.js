dae2ef346fe5d3c8815a44b52a7a16dd042e17c8
ravi.ss <ravi@scaleswift.com>
2018-06-11 13:06:08 +0530

    create, new email and cerate, new message

---
+++ public/js/index.js
@@ -0,0 +1,30 @@
+var socket = io();
+
+  socket.on('connect', function()  {
+      console.log('Connected to server');
+      
+      socket.emit('createEmail',{
+          to:'example@soc.com',
+          text: 'test soc example'
+
+      });
+
+      socket.emit('createMessage',{
+          from: 'Someone',
+          text:'regards',
+      });
+
+     
+  });
+
+  socket.on('disconnect', function() {
+      console.log('Disconnected');
+  });
+
+  socket.on('newEmail', function(email) {
+      console.log("New Email" ,email);
+  });
+
+  socket.on('newMessage', function(message) {
+    console.log("New Message",message);
+  });
--- server/server.js
+++ server/server.js
@@ -14,10 +14,33 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newEmail', {
+      from: 'example.com',
+      text: 'example text',
+      createdat: 12.00
+  });
+  socket.emit('newMessage', {
+      
+    from: 'CapX',
+    text: 'Hey',
+    createdAt:12.58
+});
+
+  socket.on('createEmail', (Email) => {
+    console.log('createEmail to back', Email);
+  });
+
+
+   socket.on('createMessage', (Message) => {
+     console.log("CreateMessage to back", Message);
+   });
+
   socket.on('disconnect', () => { 
     console.log('User was disconnected');
   });
 });
+
+
 
 server.listen(port, () => {
   console.log(`Server is up on ${port}`);
